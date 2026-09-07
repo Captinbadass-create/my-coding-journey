d9c37ad0aee85a3ed7b12b5e6a4e12fb77f884a3
Captinbadass-create <anthonyonyenegecha@gmail.com>
2026-09-07 08:17:13 +0100

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,25 @@
+
+# 1. Variables & Data Types
+user_name = "Adrian"
+user_age = 23
+hourly_rate = 74.50
+is_enrolled = True
+
+# 2. Formatting & Printing
+print("--- USER PROFILE ---")
+print("Name:", user_name)
+print("Age:", user_age)
+print("Hourly Rate: $", hourly_rate)
+print("Is Enrolled:", is_enrolled)
+print("--------------------")
+
+# 3. Dynamic Typing Challenge
+user_age = "Twenty-Five"
+print("Updated Age:", user_age)
+print("Note: Python allows changing user_age from an int to a str because it uses dynamic typing.")
+
+# 4. Calculation & Output
+hours_worked = 40
+total_earnings = hourly_rate * hours_worked
+print("Hours Worked:", hours_worked)
+print("Total Earnings: $", total_earnings)
